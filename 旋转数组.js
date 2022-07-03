305135b44095e680887bf0f5846db7592d2b5639
mac <mac@xxxxxxxxxxxxxxxxxxx.local>
2022-07-03 19:41:53 +0800

    完成旋转数组

--- 旋转数组.js
+++ 旋转数组.js
@@ -7,4 +7,21 @@
 
 const data = [1, 2, 3, 4, 5, 6, 7]
 
-function rotateArray() {}
+// 思路从数组的后面开始截取k步
+// 从数组后前面开始截取k到末尾
+// contack 连接
+// 用空间换时间
+
+function rotateArray(arr, k) {
+  if (arr.length === 0 || !k) {
+    return
+  }
+  const step = Math.abs(k % arr.length)
+  const part1 = arr.slice(-step)
+  const part2 = arr.slice(0, arr.length - step)
+  return part1.concat(part2)
+}
+
+const test = rotateArray(data, 3)
+
+console.log("test", test)
